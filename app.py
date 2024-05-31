5ff1d4959ca3393befd62186839f07966af80e0c
Nekoiii <10nekosa@gmail.com>
2024-05-31 23:58:29 +0900

    test render deploy

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-import os
-from app import app
+# import os
+# from app import app
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 5001))
+#     app.run(host="0.0.0.0", port=port)
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "Hello, World!"

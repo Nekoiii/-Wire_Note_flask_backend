9e77a195b79a733d5706a08d55bbf8e63d3f76f1
Nekoiii <10nekosa@gmail.com>
2024-05-31 20:50:54 +0900

    audio_to_text

---
+++ app/audio_to_text.py
@@ -0,0 +1,23 @@
+from flask import jsonify
+import torch
+import librosa
+from transformers import Wav2Vec2Processor, Wav2Vec2ForCTC
+
+processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")
+model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
+
+
+def audio_to_text(request):
+    audio_file = request.files["file"]
+    audio, rate = librosa.load(audio_file, sr=16000)
+
+    input_values = processor(
+        audio, return_tensors="pt", sampling_rate=16000
+    ).input_values
+    with torch.no_grad():
+        logits = model(input_values).logits
+
+    predicted_ids = torch.argmax(logits, dim=-1)
+    transcription = processor.batch_decode(predicted_ids)[0]
+
+    return jsonify({"transcription": transcription})
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,14 @@
-from flask import request, jsonify
+from flask import request
 from app import app
 from app.img_to_text import img_to_text
+from app.audio_to_text import audio_to_text
 
 
 @app.route("/img_to_text", methods=["POST"])
 def img_to_text_route():
     return img_to_text(request)
+
+
+@app.route("/audio_to_text", methods=["POST"])
+def audio_to_text_route():
+    return audio_to_text(request)
